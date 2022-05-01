a3ff1dd5a639d7d116c74bd0c97ea3377ae9d735
Vitalii Mytenko <vetalmit@gmail.com>
2022-05-01 21:25:24 +0300

    Add data validators

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field
 
 
 class ArticleBase(BaseModel):
-    text: str
-    image_url: str
-    language_code: str
+    text: str = Field(..., min_length=50, max_length=1024)
+    image_url: HttpUrl = Field(..., title="Image URL")
+    language_code: str = Field(..., max_length=3, min_length=2)
 
     class Config:
         orm_mode = True
@@ -19,10 +19,10 @@ class ArticleEdit(ArticleBase):
 
 
 class UserBase(BaseModel):
-    telegram_id: int
-    username: str
-    pet_name: str
-    language_code: str
+    telegram_id: int = Field(..., ge=0)
+    username: str = Field(..., max_length=50)
+    pet_name: str = Field(..., max_length=50)
+    language_code: str = Field(..., max_length=3, min_length=2)
 
     class Config:
         orm_mode = True
